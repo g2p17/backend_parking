be23fc06ed812b438abcd67c51f54647acfba07d
Toya-Vargas <toyavaroa@gmail.com>
2021-11-18 17:04:00 -0500

    Parking CRUD Basic -views, serializers and URLs

---
+++ parking_customers/views/parkingView.py
@@ -0,0 +1,44 @@
+from django.db.models.query import QuerySet
+from rest_framework                                     import generics
+from rest_framework                                     import status, views
+from rest_framework.response                            import Response
+from parking_customers.models.parking                   import Parking
+from parking_customers.serializers.parkingSerializer    import ParkingSerializer
+
+class ParkingCreateView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = ParkingSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+class ParkingDetailView(generics.RetrieveAPIView):
+    queryset           = Parking.objects.all()
+    serializer_class   = ParkingSerializer
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+class ParkingUpdateView(generics.UpdateAPIView):
+    queryset           = Parking.objects.all()
+    serializer_class   = ParkingSerializer
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset           = Parking.objects.all()
+    serializer_class   = ParkingSerializer
+    def delete(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+
+'''
+class ListParkingView(generics.ListAPIView):
+    queryset           = Parking.objects.all()
+    serializer_class   = ParkingSerializer
+    
+    def get_queryset(self):
+        queryset = Parking.objects.filter(origin_account_id=self.kwargs['account'])
+        #queryset = Parking.objects.filter(origin_account_id=self.kwargs['account'])
+        return queryset
+
+'''
