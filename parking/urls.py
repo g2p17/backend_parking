60450f01da4d980d978798eec4179a3e44ce5e05
Toya-Vargas <toyavaroa@gmail.com>
2021-11-19 10:29:58 -0500

    GP-37 Update Parking Model with PositiveSmallIntegerField and Microservices´s swagger documentation

--- parking/urls.py
+++ parking/urls.py
@@ -24,9 +24,9 @@ from parking_customers  import views as parkingView
 
 schema_view = get_schema_view(
       openapi.Info(
-         title="Snippets API",
-         default_version='v1',
-         description="Test description",
+         title="Parking lots API",
+         default_version='v1.0',
+         description="This microservice get parking lots info",
          terms_of_service="https://www.google.com/policies/terms/",
          contact=openapi.Contact(email="contact@snippets.local"),
          license=openapi.License(name="BSD License"),
